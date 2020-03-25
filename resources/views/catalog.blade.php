33f89179a959b61cbc352e732c79d90209583185
Alexandre.Bliard <alexandre.bliard@le-campus-numerique.fr>
2020-03-25 10:13:40 +0100

    mobile clear2 - go to desktop

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Expanded&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Encode+Sans&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
